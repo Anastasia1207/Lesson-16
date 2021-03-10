c3d1d098cd2554abc3600b4053ce6a5d0941b6b0
Anastasija <78434027+Anastasia1207@users.noreply.github.com>
2021-03-10 15:33:05 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+ const name = 'Anastasia';
+ const city = 'Riga';
+ const number = 12;
+
+console.log(`Hello, my name is ${name}, I live in ${city} and my favorite number is ${number}`);
+
+// ----------------------------------------------------------- //
+
+
+let cars = ['Maz',' Gaz','Zaz','Vaz','ferrari'];
+console.log('5th car is:' + cars[4]);
+console.log('4th car is:' + cars[3]);
+console.log('3rd car is:' + cars[2]);
+console.log('2nd car is:' + cars[1]);
+console.log('1st car is:' + cars[0]);
+
+
+
+const country = 'Italy';
+const capitalCity = 'Rome';
+const cityPopulation = '2.873 million';
+
+console.log(`${capitalCity}, is the capital of ${country} with population in ${cityPopulation}.`);
